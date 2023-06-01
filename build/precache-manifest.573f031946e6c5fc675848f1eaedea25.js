25186e25247fb7a9610ad2ade34d65a924716a30
Karol Brennan <8312289+karolbrennan@users.noreply.github.com>
2023-06-01 09:08:32 -0500

    run build, update donation links

---
+++ build/precache-manifest.573f031946e6c5fc675848f1eaedea25.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9963deaad876fa81445673694bdaea02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0821ece51572e1990663",
+    "url": "/static/css/2.75b42941.chunk.css"
+  },
+  {
+    "revision": "d77b53a557716e1603c7",
+    "url": "/static/css/main.fa061f06.chunk.css"
+  },
+  {
+    "revision": "0821ece51572e1990663",
+    "url": "/static/js/2.764857b0.chunk.js"
+  },
+  {
+    "revision": "78f25236c37fc20bcef8a9a310ca6cda",
+    "url": "/static/js/2.764857b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d77b53a557716e1603c7",
+    "url": "/static/js/main.cdf498c8.chunk.js"
+  },
+  {
+    "revision": "b024edaa8276bd793f0f",
+    "url": "/static/js/runtime-main.9a433e25.js"
+  },
+  {
+    "revision": "15f4a71d52079b750907061a4a7fc173",
+    "url": "/static/media/buyacoffee.15f4a71d.png"
+  },
+  {
+    "revision": "5a6f7124fc364ee38beca9cf869cec60",
+    "url": "/static/media/logo.5a6f7124.svg"
+  },
+  {
+    "revision": "e1869679b20c6fa6e2ee94780779960c",
+    "url": "/static/media/paypalme.e1869679.jpeg"
+  },
+  {
+    "revision": "0bfe8021aac5fa800400b8e86d2345f4",
+    "url": "/static/media/venmo.0bfe8021.jpeg"
+  }
+]);
